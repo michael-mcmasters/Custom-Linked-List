f7ee3f3f23bbc17633f0d73527df459516451abf
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-06 18:12:15 -0500

    Initial commit

---
+++ src/com/company/linkedlist/LinkedList.java
@@ -0,0 +1,24 @@
+package com.company.linkedlist;
+
+public class LinkedList {
+
+    public Node head;
+
+    public LinkedList() {
+
+    }
+
+    public Node getHead() {
+        return null;
+    }
+
+    public Node getIndex(Integer index) {
+        return null;
+    }
+
+    // Loop through every node and check node.getNext().
+    // If getNext() is null, it is the last node in the list.
+    public Node getTail() {
+        return null;
+    }
+}
---
+++ src/com/company/linkedlist/Node.java
@@ -0,0 +1,13 @@
+package com.company.linkedlist;
+
+public class Node {
+    private Node nextNode;
+    private String value;
+
+    public Node(Node nextNode, String value) {
+        this.nextNode = nextNode;
+        this.value = value;
+    }
+
+
+}
