0db66254843d82ec0df16b12850884310bf3a508
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-07 13:42:36 -0500

    Create 3 constructor tests

--- src/test/java/com/company/customlinkedlist/linkedlist/LinkedListTest.java
+++ src/test/java/com/company/customlinkedlist/linkedlist/LinkedListTest.java
@@ -7,6 +7,46 @@ import org.junit.jupiter.api.Assertions;
 public class LinkedListTest {
 
     @Test
+    public void constructorTestA() {
+        // Given
+        LinkedList<String> list = new LinkedList<>();
+
+        // When
+        int expected = 0;
+        int actual = list.size();
+
+        // Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void constructorTestB() {
+        // Given
+        LinkedList<String> list = new LinkedList<>("Peter", "Kevin", "Angelino");
+
+        // When
+        int expected = 3;
+        int actual = list.size();
+
+        // Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void constructorTestC() {
+        // Given
+        String[] names = { "Peter", "Kevin", "Angelino", "Jackson" };
+        LinkedList<String> list = new LinkedList<>(names);
+
+        // When
+        int expected = 4;
+        int actual = list.size();
+
+        // Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
     public void addTest() {
         // Given
         LinkedList<String> list = new LinkedList<>();
@@ -20,4 +60,23 @@ public class LinkedListTest {
         // Then
         Assertions.assertEquals(expected, actual);
     }
+
+
+
+
+
+
+
+
+
+    //    @Test
+//    public void constructorTest() {
+//        // Given
+//        LinkedList<String>list = new LinkedList<>();
+//
+//        // When
+//
+//        // Then
+//          Assertions.assertEquals(expected, actual);
+//    }
 }
