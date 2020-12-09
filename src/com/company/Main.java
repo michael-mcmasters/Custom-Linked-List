73fc748b1323637f4b1522fc9228d962a95e030c
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-09 16:23:54 -0500

    Remove unused imports

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,11 @@ package com.company;
 
 import com.company.linkedlist.LinkedList;
 
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-
+        LinkedList<String> list = new LinkedList();
+        list.add("arg1", "arg2", "arg3");
+        System.out.println(list.size());
     }
 }
